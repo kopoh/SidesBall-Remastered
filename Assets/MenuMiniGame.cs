03a031909b8d69b7e961aca8c3ca2d26b4a740a9
kopoh <40755137+kopoh@users.noreply.github.com>
2021-06-13 17:06:59 +0300

    13.06.2021

---
+++ Assets/MenuMiniGame.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class MenuMiniGame : MonoBehaviour
+{
+    [Range(1, 10)] public float Delta;
+    public GameObject StartObj;
+    public GameObject FinishObj;
+
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MenuGameSphere"))
+        {
+            if (transform.position.y <= FinishObj.transform.position.y + 1)
+            {
+                other.transform.position = new Vector3(Random.Range(-14, 14), StartObj.transform.position.y,
+                    StartObj.transform.position.z);
+            }
+        }
+        
+        Debug.Log("хуй" + other.tag);
+    }
+    
+}
---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public Image loadingImg;
+ 
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+        //StartCoroutine(AsyncLoad(SceneName));
+    }
+
+    IEnumerator AsyncLoad(string SceneName) 
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(SceneName);
+        while(!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            loadingImg.fillAmount = progress;
+            yield return null;
+        }
+    }
+
+    public void LoadURL(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
